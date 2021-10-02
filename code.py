196f34c5110b84da962891d9c7c595451494030e
Jayasankar J N <55084240+Jayasankar-JN@users.noreply.github.com>
2021-10-02 22:57:55 +0530

    Added extracting words from each line

--- code.py
+++ code.py
@@ -56,15 +56,38 @@ for i in contours:
     lines.append(org_img[i[0][0][1]:i[1][0][1],:])
 
 
+#extracting words from each line 
+words = []
+for i in lines:
+    img_array = i//255
+    img_array = abs(img_array - 1)
+    img_array = img_array.sum(axis = 0)
+    m_img = i.copy()
+    for j in range(len(img_array)):
+        if(img_array[j] != 0):
+            m_img = cv2.line(m_img,(j,0),(j,i.shape[0]),(0,255,0),1)
+    # Invert colors for contour detection
+    m_img = 255 - m_img
+    contours, hierarchy = cv2.findContours(m_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(m_img,contours,-1,(0,255,0),4)
+    for j in contours:
+        words.append(i[:,j[0][0][0]:j[2][0][0]])
 
 
-
+#Assigning directory to lines,words 
 
 directory1 = './Lines/Line_'
+directory2 = './Words/Word_'
+
+
 
 
 for i in range(len(lines)):
     cv2.imwrite(directory1 + str(i) + ".png",lines[i])
+
+
+for i in range(len(words)):
+    cv2.imwrite(directory2 + str(i) + ".png",words[i])
 
 
 
