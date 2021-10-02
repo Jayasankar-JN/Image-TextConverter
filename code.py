9bc8f9c1237b6cb0ab1b06a2187c5fae4a71c114
codehat10 <72185074+codehat10@users.noreply.github.com>
2021-10-02 22:50:02 +0530

    Initial commit

---
+++ code.py
@@ -0,0 +1,72 @@
+#importing libraries
+import cv2
+import numpy as np
+import os
+import math
+
+#reading image and load it in grayscale mode
+file_name = input("Enter the Image filename (in png format) : ")
+org_img = cv2.imread(file_name,0)
+img = org_img.copy()
+
+# removing background of image
+
+#floor function returns the greatest integer less than or equal to input
+n = math.floor(img.shape[0]/2)
+m = math.floor(img.shape[1]/2)
+pt1 = 0
+pt2 = 0
+for i in range(m):
+    if(img[n][i] == 255):
+        pt1 = i
+        break
+for i in range(n):
+    if(img[i][m] == 255):
+        pt2 = i
+        break
+# 3 -->> offset
+img = img[pt1+3:-(pt1+3),pt2+3:-(pt2+3)]
+org_img = img.copy()
+
+
+# highlighting image
+img = (img//255)*255
+
+
+# making array
+img_array = img//255
+img_array = abs(img_array - 1)
+img_array = img_array.sum(axis = 1)
+
+# darkening lines for drawing contours
+m_img = img.copy()
+for i in range(len(img_array)):
+    if(img_array[i] != 0):
+        m_img = cv2.line(m_img,(img.shape[1],i),(0,i),(0,255,0),2)
+
+# Invert colors for contour detection
+m_img = 255 - m_img
+
+# Detect contours
+contours, hierarchy = cv2.findContours(m_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+#make list of lines
+lines = []
+for i in contours:
+    lines.append(org_img[i[0][0][1]:i[1][0][1],:])
+
+
+
+
+
+
+directory1 = './Lines/Line_'
+
+
+for i in range(len(lines)):
+    cv2.imwrite(directory1 + str(i) + ".png",lines[i])
+
+
+
+
+print("Outputs are sucessfully placed inside the corresponding directories!")
